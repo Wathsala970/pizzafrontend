9913c70891e935e51fad961d0c553684935d54cc
Wathsala Suriyanpola <100006695+Wathsala970@users.noreply.github.com>
2023-06-22 09:56:36 +0530

    Update userAction.js

--- actions/userAction.js
+++ actions/userAction.js
@@ -5,7 +5,7 @@ export const registerUser = (user)=> async dispatch=>{
     dispatch({type: 'USER_REGISTER_REQUEST'})
 
     try {
-        const response = await axios.post('/api/users/register', user)
+        const response = await axios.post('https://online-pizza-order.onrender.com/api/users/register', user)
         console.log(response);
         dispatch({type: 'USER_REGISTER_SUCCESS'})
     } catch (error) {
@@ -18,7 +18,7 @@ export const loginUser = (user)=> async dispatch=>{
     dispatch({type: 'USER_LOGIN_REQUEST'})
 
     try {
-        const response = await axios.post('/api/users/login', user)
+        const response = await axios.post('https://online-pizza-order.onrender.com/api/users/login', user)
         console.log(response);
         dispatch({type: 'USER_LOGIN_SUCCES', payload: response.data})
         localStorage.setItem('currentUser', JSON.stringify(response.data))
